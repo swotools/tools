345bd6674a796e237010600a91711ab2beb2b2a7
AlfaPapa <papa@alfa.com>
2017-07-17 20:05:24 +0200

    Puliso solo file di swog

--- bin/swog/cleanpack.sh
+++ bin/swog/cleanpack.sh
@@ -2,8 +2,8 @@
 set -e
 source vendor/swolab/swotools/bin/_config.sh
 # Controllo SWOG
-# se lo chiami da composer swo:clean, si autoelimina cancellando vendor
-SWOG_COPY=( "package.json" "gruntfile.js" "grunt" "scss" "js" )
+# 17-7-17 riduco funzioni lasciando solo il reset di swog
+
 for i in "${SWOG_COPY[@]}"
 do
   echo "Elimino $i ..."
@@ -11,34 +11,3 @@ do
       rm -rf $i
   fi
 done
-
-for i in "${DIRCLEARCACHE[@]}"
-do
-  echo "Elimino $i ..."
-  if [ -d $i ] || [ -f $i ];then
-      rm -rf $i
-  fi
-done
-
-for i in "${SWO_DIRS[@]}"
-do
-  echo "Elimino $i ..."
-  if [ -d $i ] || [ -f $i ];then
-      rm -rf $i
-  fi
-done
-
-## SOLO X POCKET REPO !!
-# rm -rf dist
-# rm -rf node_modules
-# rm -rf swog
-# rm -rf tasks
-# rm -rf scss
-# rm -rf js
-# rm gruntfile.js
-# rm package.json
-# rm composer.lock
-# rm -rf vendor
-# rm -rf logs
-# rm -rf tmp
-# rm -rf cache
