8ddee23e265851767c6ecd3d775f4f0bed0a3057
AlfaPapa <papa@alfa.com>
2017-10-06 18:05:23 +0200

    fix apici dati statici

--- bin/_config.sh
+++ bin/_config.sh
@@ -4,23 +4,23 @@ USER=$(whoami)
 OSNAME=$(uname)
 NOW=$(date +"%m-%d-%Y-%H-%M-%S");
 COMPOSERFILE="composer.json";
-DIRBIN="bin";
+DIRBIN='bin';
 DIRBASH="$DIRBIN/bash";
 WWW_USER=`ps axo user,group,comm | egrep '(apache|httpd)' | grep -v ^root | uniq | cut -d\  -f 1`
 
-DIRLOGS="logs";
-DIRCACHE="cache";
+DIRLOGS='logs';
+DIRCACHE='cache';
 DIRSTATIC='public/static'
-DIRTMP="tmp";
-DIRAPP="app";
+DIRTMP='tmp';
+DIRAPP='app';
 DIRVENDOR='vendor'
 DIRNODEMOD='node_modules'
 COMPOSERLOCK='composer.lock'
 
-SWOGBASHINSTALL="swoginstall.sh"
-DIRSWOG="vendor/swotools/swog"
-SWOG_COPY=( "package.json" "gruntfile.js" "grunt" "scss" "js" )
+SWOGBASHINSTALL='swoginstall.sh'
+DIRSWOG='vendor/swotools/swog'
+SWOG_COPY=( 'package.json' 'gruntfile.js' 'grunt' 'scss' 'js' )
 
-ENVFILE=".env";
+ENVFILE='.env';
 SWO_DIRS=("$DIRLOGS" "$DIRTMP" "$DIRCACHE" "$DIRSTATIC")
 RESETFILES=("$DIRNODEMOD" "$DIRVENDOR" "$COMPOSERLOCK")
